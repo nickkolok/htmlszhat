29996943f8d1b80b3ca21ad86e712abeeb4f9f00
nickkolok <nickkolok@mail.ru>
2016-10-19 20:53:12 +0300

    В скрипт тестирования добавлены *.htmlorig

--- test.sh
+++ test.sh
@@ -7,9 +7,11 @@ do
 
 	tar -zcvf  $html.gz.tar.gz             $html
 	tar -zcvf  $html.gz.htmlszhat.tar.gz   $html.htmlszhat
+	tar -zcvf  $html.gz.htmlorig.tar.gz    $html.htmlorig
 
 	tar -cvjf  $html.bz2.tar.bz2           $html
 	tar -cvjf  $html.bz2.htmlszhat.tar.bz2 $html.htmlszhat
+	tar -cvjf  $html.bz2.htmlorig.tar.bz2  $html.htmlorig
 
 done;
 
